6ec6f7f06cdc0e0f2e397ec91c02e4ee07fd6cc3
rsundone <pavel.serhiichuk@gmail.com>
2022-08-18 15:21:39 +0300

    reload refactor

---
+++ src/components/CategoryProducts/CategoryProducts.style.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const ProductsWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  height: calc(100vh - 130px);
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-content: start;
+  gap: 10px;
+  margin: 10px;
+  overflow: scroll;
+`
